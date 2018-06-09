29b0cd520e03a9f415f70ed7833bc8f0a6177aa9
lukasztecza <lukasztecza.pl@gmail.com>
2018-06-09 11:59:53 -0400

    interface fix

--- src/Model/Repository/DatabaseConnectionInterface.php
+++ src/Model/Repository/DatabaseConnectionInterface.php
@@ -6,7 +6,7 @@ interface DatabaseConnectionInterface
     public function prepare(string $sql) : void;
     public function clean() : void;
     public function fetch(string $sql = null, array $arguments = []) : array;
-    public function execute(string $sql = null, $arguments) : string;
+    public function execute(string $sql = null, array $arguments = []) : string;
     public function begin() : void;
     public function commit() : void;
     public function rollBack() : void;
